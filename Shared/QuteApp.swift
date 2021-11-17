944373d5bdd5c2076de2d654e2904579bfe8356b
Davide Biancardi <davidebiancardi97@gmail.com>
2021-11-17 10:53:46 +0100

    Initial Commit

---
+++ Shared/QuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuteApp.swift
+//  Shared
+//
+//  Created by Davide Biancardi on 17/11/21.
+//
+
+import SwiftUI
+
+@main
+struct QuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
