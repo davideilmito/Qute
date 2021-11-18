61208a4daf59a0af45ed19152f93bfc6d297d47a
MatildeCotroneo <matildecotroneo1998@gmail.com>
2021-11-18 11:05:21 +0100

    ho editato la pet card, aggiungendo la foto corretta e editando il testo (carattere maggiore)

--- Shared/PetCardView.swift
+++ Shared/PetCardView.swift
@@ -15,11 +15,18 @@ struct PetCardView: View {
                         .font(.system(size: 40, weight: .bold, design: .rounded))
                         .fontWeight(.bold)
                         .padding()
-                    Image("pug")
+                    
+                    Image ("cane")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 355, height: 240)
+                            .overlay(RoundedRectangle(cornerRadius: 60).stroke(Color(red: 143/255, green: 149/255, blue: 211/255),lineWidth: 6))
+                      
                 }
-                Text("Dogs are known to be faithful and loyal companions,they’re good family pets as they’re affectionate, gentle and fiercely protective of their owners.                            They possess patience, intelligence and gentleness,so they can practice activities with humans, letting them relax and stress out.")
-            }
+                Text("Dogs are known to be faithful and loyal companions,they’re good family pets as they’re affectionate, gentle and fiercely protective of their owners.                                      They possess patience, intelligence and gentleness,so they can practice activities with humans, letting them relax and stress out.")
+            } .font(.system(size: 20))
             .padding(.all, 40)
+            
             Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                 Text("Button")
                     .padding(.all, 35)
