254709ff46ee1739a9c7ec17da7736d79f4e99aa
Davide Biancardi <davidebiancardi97@gmail.com>
2021-11-23 17:06:42 +0100

    Aggiunta Onboarding screen

--- Shared/Home.swift
+++ Shared/Home.swift
@@ -12,7 +12,14 @@ import SwiftUI
 
 struct HomeView: View {
     
-   
+    
+    @AppStorage("ShowOnBoarding") var showOnBording = true
+
+    init() {
+        UIPageControl.appearance().currentPageIndicatorTintColor = UIColor(  Color(red: 143/255, green: 149/255, blue: 211/255))
+       UIPageControl.appearance().pageIndicatorTintColor = UIColor.black
+       }
+    
     var body: some View {
         
         TabView{
@@ -46,8 +53,115 @@ struct HomeView: View {
                 }
         
         }.accentColor(Color(red: 143/255, green: 149/255, blue: 211/255))
+            .fullScreenCover(isPresented: $showOnBording, content: {OnboardingView(showOnBoarding: $showOnBording)})
+        
     }
+
+
 }
+
+
+
+
+
+struct OnboardingView: View{
+    
+    @Binding var showOnBoarding : Bool
+    
+    var body: some View{
+        
+        
+        TabView{
+            
+           PageView(title: "Primo", subtitle: "Sottotitolo", imageName: "pawprint.fill",showDismissedButton: false,showOnBoarding: $showOnBoarding)
+              
+
+            PageView(title: "Secondo", subtitle: "Sottotitolo", imageName: "pawprint",showDismissedButton: false,showOnBoarding: $showOnBoarding)
+                
+
+            PageView(title: "Terzo", subtitle: "Sottotitolo", imageName: "bookmark",showDismissedButton: false,showOnBoarding: $showOnBoarding)
+               
+
+            PageView(title: "Quarto", subtitle: "Sottotitolo", imageName: "house",showDismissedButton: true,showOnBoarding: $showOnBoarding)
+               
+            
+        } // TabView
+        .tabViewStyle(PageTabViewStyle())
+        
+
+    }
+    
+}
+
+
+struct PageView: View {
+    
+    let title : String
+    let subtitle: String
+    let imageName: String
+    let showDismissedButton: Bool
+    @Binding var showOnBoarding : Bool
+    
+    var body: some View{
+        
+        
+        
+        VStack{
+            
+            
+            Image(systemName: imageName)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 150, height: 150)
+                .padding()
+                .foregroundColor(Color(red: 143/255, green: 149/255, blue: 211/255))
+                
+                
+                
+            
+            Text(title)
+                .font(.system(size: 42, weight: .bold, design: .rounded))
+                .padding()
+                
+            
+            Text(subtitle)
+                .font(.system(size: 30, weight:.light, design: .rounded))
+                .foregroundColor(Color.secondary)
+            
+                .padding()
+        
+            if showDismissedButton{
+                
+                Button(action: {showOnBoarding.toggle()}){
+                    
+                    
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundColor(Color(red: 143/255, green: 149/255, blue: 211/255))
+                        .frame(width: 200, height: 50, alignment: .center)
+                        .overlay(Text("Get started!")
+                        .font(.system(size: 20, weight: .medium, design: .rounded))
+                        .padding()
+                        .foregroundColor(.white))
+                }
+                
+                
+            }
+            
+            
+            
+        
+        } //VStack
+        .frame(
+              minWidth: 0,
+              maxWidth: .infinity,
+              minHeight: 0,
+              maxHeight: .infinity
+            )
+    }
+        
+}
+
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
